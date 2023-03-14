3804ff097f4f5acecb1451ac7165b6f7a66dbdf4
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-14 14:22:31 +0100

    can select two options, however did not manage to get it working using outside function/method

--- 14-3/Practice/Izbornik/Program.cs
+++ 14-3/Practice/Izbornik/Program.cs
@@ -19,9 +19,33 @@ namespace Izbornik
             string kraj = (cijenaD <= racunD) ? "\nIznos na racunu je dovoljan i opcija je uspjesno aktivirana!" : "\nIznos na racunu je nedovoljan za aktivaciju.";
             Console.WriteLine(kraj);
         }
+
+        /*static void simpaDaNe1() // rad u tijeku, pokusaj prebacivanja da svi caseovi se vrte u biti kao odvojena funkcija, da bude preglednija switch petlja
+        {
+            //Console.WriteLine($"\nZelite aktivirati {opcija} po cijeni {cijena} EUR, odgovori s DA. Opcija ukljucuje blablabla. Odgovori NE za povratak na prijasnji izbornik.\n");
+
+            string simpaDaNe = Console.ReadLine();
+            string simpaDaNeL = simpaDaNe.ToLower();
+            switch (simpaDaNeL)
+            {
+                case "da":
+                    Izracun();
+                    break;
+
+                case "ne":
+                    goto simpa;
+
+                default:
+                    Console.WriteLine("\nNepoznat unos. Pokusajte ponovo.");
+                    goto simpa;
+                    break;
+            }
+            
+
+        }*/
         static void Main(string[] args)
         {
-            string[] mainIzbornik = { "1. Simpa opcije", "2. Zmajska", "3. Internet", "4. Televizija i glazba", "5. Roaming Surf", "6. VoLTE i VoWIFI", "7. Ostalo" };
+            string[] mainIzbornik = { "1. Simpa opcije", "2. Zmajska", "3. Internet (ne radi)", "4. Televizija i glazba (ne radi)", "5. Roaming Surf (ne radi)", "6. VoLTE i VoWIFI (ne radi)", "7. Ostalo (ne radi)" };
             izbornik:
             Console.WriteLine("Posaljite broj pokraj grupe opcija koju zelite aktivirati.\n");
             int i = 0;
@@ -45,7 +69,7 @@ namespace Izbornik
                 case "1":
                     simpa:
                         Console.WriteLine("\nPosaljite broj opcije koju zelite aktivirati:\n");
-                        string[] simpaIzbornik = { "1. Simpa L", "2. Simpa M", "3. Simpa S", "4. Simpa XS" };
+                        string[] simpaIzbornik = { "1. Simpa L", "2. Simpa M (rad u tijeku)", "3. Simpa S (ne radi)", "4. Simpa XS (ne radi)" };
                         int j = 0;
                         do
                         {
@@ -55,7 +79,6 @@ namespace Izbornik
                         while (j < simpaIzbornik.Length);
 
                         string simpaIzbor = Console.ReadLine();
-                   // test:
                         switch (simpaIzbor)
                         {
                             case "1":
@@ -81,7 +104,7 @@ namespace Izbornik
                                 break;
 
                             case "2":
-                                Console.WriteLine("\nZelite aktivirati Simpa M po cijeni 12,47 EUR, odgovori s DA. Opcija ukljucuje blablabla. Odgovori NE za povratak na prijasnji izbornik.\n");
+                                Console.WriteLine("\nZelite aktivirati Simpa M po cijeni 11,81 EUR, odgovori s DA. Opcija ukljucuje blablabla. Odgovori NE za povratak na prijasnji izbornik.\n");
 
                                 string simpaIzbor2 = Console.ReadLine();
                                 string simpaIzbor2L = simpaIzbor2.ToLower();
@@ -100,16 +123,90 @@ namespace Izbornik
                                 }
                                 break;
 
+                            /*case "3":
+                            string opcija = "Simpa M";
+                            string cijena = "11.81";
+
+                            simpaDaNe1();*/
+
                             default:
                             Console.WriteLine("\nNepoznat unos. Pokusajte ponovo.");
                             goto izbornik;
-                    }
-
+                            break;
+                        }
                         break;
-                }
-            //}
+                case "2":
+                    zmajska:
 
-            Console.ReadLine();
+                        Console.WriteLine("\nPosaljite broj opcije koju zelite aktivirati:\n");
+                        string[] zmajIzbornik = { "1. Zmaj", "2. Zmajic" };
+                        int h = 0;
+                        do
+                        {
+                            Console.WriteLine(zmajIzbornik[h]);
+                            h++;
+                        }
+                        while (h < zmajIzbornik.Length);
+
+                        string zmajIzbor = Console.ReadLine();
+                        switch (zmajIzbor)
+                        {
+                        case "1":
+                            Console.WriteLine("\nZelite aktivirati opciju Zmaj po cijeni 10,27 EUR, odgovori s DA. Opcija ukljucuje blablabla. Odgovori NE za povratak na prijasnji izbornik.\n");
+
+                            string zmajIzbor1 = Console.ReadLine();
+                            string zmajIzbor1L = zmajIzbor1.ToLower();
+                            switch (zmajIzbor1L)
+                            {
+                                case "da":
+                                Izracun();
+                                break;
+
+                                case "ne":
+                                goto zmajska;
+
+                                default:
+                                    Console.WriteLine("\nNepoznat unos. Pokusajte ponovo.");
+                                    goto zmajska;
+                            }
+                            break;
+
+                            case "2":
+                            Console.WriteLine("\nZelite aktivirati opciju Zmajic po cijeni 8,81 EUR, odgovori s DA. Opcija ukljucuje blablabla. Odgovori NE za povratak na prijasnji izbornik.\n");
+
+                            string zmajIzbor2 = Console.ReadLine();
+                            string zmajIzbor2L = zmajIzbor2.ToLower();
+                            switch (zmajIzbor2L)
+                            {
+                                case "da":
+                                Izracun();
+                                break;
+
+                                case "ne":
+                                goto zmajska;
+
+                                default:
+                                    Console.WriteLine("\nNepoznat unos. Pokusajte ponovo.");
+                                goto zmajska;
+                            }
+                            break;
+
+                /*case "3":
+                string opcija = "Simpa M";
+                string cijena = "11.81";
+
+                simpaDaNe1();*/
+
+                default:
+                    Console.WriteLine("\nNepoznat unos. Pokusajte ponovo.");
+                    goto izbornik;
+                    break;
+            }
+            break;
+        }
+        //}
+
+        Console.ReadLine();
         }
     }
 }
