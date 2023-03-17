c2ba9967f06e531db8326377114ab7e182a03619
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-17 11:58:32 +0100

    can create a new file and copy contents from the original to it using stream

--- 17-3/Practice/MakeWriteCopyFile/Program.cs
+++ 17-3/Practice/MakeWriteCopyFile/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using PdfSharp;
@@ -19,12 +20,39 @@ namespace MakeWriteCopyFile
                 Directory.CreateDirectory(directoryPath);            
             }
 
-            string fileName = "D:\\BEKEND\\MonoElprosCsharp\\17-3\\test123\\abc.txt";
+            string filePath = "D:\\BEKEND\\MonoElprosCsharp\\17-3\\test123\\abc.txt";
 
-            if (!File.Exists(fileName)) 
+            if (!File.Exists(filePath)) 
             {
-                File.Create(fileName);
+                FileStream stream = File.Create(filePath);
+                stream.Flush();
+                stream.Close();
             }
+
+
+            ///// tu mi je napravio folder i prazan file *\test123\abc.txt
+
+            File.WriteAllText(filePath, "Hello World!"); // napisao je u fajlu Hello World
+
+            string tekst = File.ReadAllText(filePath);
+            Console.WriteLine(tekst); // napisao u konzoli, za sad sve oke
+
+            //////////////////
+
+            string filePath2 = "D:\\BEKEND\\MonoElprosCsharp\\17-3\\test123\\abc2.txt";
+
+            if (!File.Exists(filePath2))
+            {
+                FileStream stream = File.Create(filePath2);
+                stream.Flush();
+                stream.Close();
+            }
+            
+            FileStream startStream = File.OpenRead(filePath);
+            FileStream endStream = File.OpenWrite(filePath2);
+            startStream.CopyTo(endStream); ///// u napravljenom fajlu abc2.txt se sada nalazi Hello World!
+
+
 
             Console.Read();
         }
