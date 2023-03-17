28a5cc1c47e1616a32f2d3921b95405ddfcb4f4d
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-17 10:56:11 +0100

    can check for and create folder and file

--- 17-3/Practice/MakeWriteCopyFile/Program.cs
+++ 17-3/Practice/MakeWriteCopyFile/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PdfSharp;
 
 namespace MakeWriteCopyFile
 {
@@ -10,6 +12,21 @@ namespace MakeWriteCopyFile
     {
         static void Main(string[] args)
         {
+            string directoryPath = $"D:\\BEKEND\\MonoElprosCsharp\\17-3\\test123";
+
+            if (!Directory.Exists(directoryPath)) // default je ako vrijedi znaci true, pa napravi; sa ! znaci ako NIJE da vrijedi Exists onda napravi
+            {
+                Directory.CreateDirectory(directoryPath);            
+            }
+
+            string fileName = "D:\\BEKEND\\MonoElprosCsharp\\17-3\\test123\\abc.txt";
+
+            if (!File.Exists(fileName)) 
+            {
+                File.Create(fileName);
+            }
+
+            Console.Read();
         }
     }
 }
