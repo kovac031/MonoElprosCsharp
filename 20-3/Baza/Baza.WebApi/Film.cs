4c94eec5e2b64b057c14562e53382d0d24269962
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-20 14:34:34 +0100

    hotfix

---
+++ 20-3/Baza/Baza.WebApi/Film.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Baza.WebApi
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        List<Film> filmList = new List<Film>
+            {
+                new Film { Id = 1, Title = "Titanic" },
+                new Film { Id = 2, Title = "Hellboy"}
+            };
+
+        /*public Film(int movieId, string movieTitle) 
+        {
+            //this.Id = movieId;
+            //this.Title = movieTitle;
+
+            
+
+           // Film film = new Film(1, "Titanic");
+           // filmList.Add(film.Id = 1, film.Title = "Titanic");
+
+        }*/
+
+    }
+}
---
+++ 20-3/vjezba/vjezba.consoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba.consoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Author> authors = new List<Author>
+            {
+            new Author { Name = "Mahesh Chand", Book = "ADO.NET Programming", Price = 49.95 },
+            new Author { Name = "Neel Beniwal", Book = "Jump Ball", Price = 19.95 },
+            new Author { Name = "Chris Love", Book = "Practical PWA", Price = 29.95 }
+            };
+            try
+            {
+                foreach (Author author in authors)
+                {
+                    Console.Write($"{author.Name}\t{author.Book}\t{author.Price}\n");
+                }
+            }
+            catch (Exception ex) { }
+            Console.ReadKey();
+
+            //////////////////////////////////////////////////////////
+            ///lista ali iz druge klase
+            ///
+
+            try
+            {
+                foreach (Lista lista in Lista.mojaLista)
+                {
+                    Console.Write($"{author.Name}\t{author.Book}\t{author.Price}\n");
+                }
+            }
+            catch (Exception ex) { }
+            Console.ReadKey();
+
+
+
+        }
+    }
+    public class Author
+    {
+        // Auto-Initialized properties
+        public string Name { get; set; }
+        public string Book { get; set; }
+        public double Price { get; set; }
+        
+    }
+}
