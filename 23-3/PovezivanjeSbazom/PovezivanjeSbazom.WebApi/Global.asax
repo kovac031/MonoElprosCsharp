2da059e9f4dae799dff97beb0099c1987181fd1b
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-23 08:20:42 +0100

    copied yesterdays file to todays folder

---
+++ 23-3/PovezivanjeSbazom/PovezivanjeSbazom.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PovezivanjeSbazom.WebApi.WebApiApplication" Language="C#" %>
---
+++ 23-3/dbo.Film.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Film] (
+    [Id]       UNIQUEIDENTIFIER NOT NULL,
+    [Title]    VARCHAR (50)     NOT NULL,
+    [Release]  INT              NOT NULL,
+    [Genre]    VARCHAR (50)     NOT NULL,
+    [Duration] INT              NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
