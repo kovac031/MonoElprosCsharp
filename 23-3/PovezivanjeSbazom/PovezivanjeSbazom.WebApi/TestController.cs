6b03cda4340c60d1fe7d5cc3421ec04d771c8e0d
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-23 11:20:14 +0100

    PUT and DELETE works, finished yesterdays work, moving on to MLA

--- 23-3/PovezivanjeSbazom/PovezivanjeSbazom.WebApi/TestController.cs
+++ 23-3/PovezivanjeSbazom/PovezivanjeSbazom.WebApi/TestController.cs
@@ -164,19 +164,19 @@ namespace PovezivanjeSbazom.WebApi
                 {
                     SqlCommand cmd = new SqlCommand("INSERT INTO Film VALUES (@id, @title, @release, @genre, @duration);", conn);
 
-                    film.Id = new Guid();
-                    cmd.Parameters.AddWithValue("@id", film.Id);
+                    //film.Id = new Guid();
+                    cmd.Parameters.AddWithValue("@id", Guid.NewGuid());
                     cmd.Parameters.AddWithValue("@title", film.Title);
                     cmd.Parameters.AddWithValue("@release", film.Release);
                     cmd.Parameters.AddWithValue("@genre", film.Genre);
                     cmd.Parameters.AddWithValue("@duration", film.Duration);
                     conn.Open();
 
-                    if(cmd.ExecuteNonQuery()<0)
+                    if(cmd.ExecuteNonQuery()>0)
                     {
-                        List<FilmClass> filmList = new List<FilmClass>();
+                        //List<FilmClass> filmList = new List<FilmClass>(); // iniciram listu ali nista ne unosim u nju, trebao bi kopirati onaj GET kod za to, nema potrebe
 
-                        return Request.CreateResponse(HttpStatusCode.OK, filmList);
+                        return Request.CreateResponse(HttpStatusCode.OK, "Added movie " + film.Title +" to the list.");
                     }
                     else
                     {
@@ -189,6 +189,103 @@ namespace PovezivanjeSbazom.WebApi
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error occured while executing Post: {ex.Message}");
             }
         }//unese sta mu napisem ali ne generira guid nego unese sa 0000 sve i javi else message
+
+        [HttpPut]
+        [Route("api/test/put/{id}")] 
+        public HttpResponseMessage Put(Guid id, [FromBody] FilmClass film)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString); 
+
+                using (conn)
+                {
+                    SqlCommand cmdS = new SqlCommand("SELECT * FROM Film WHERE Id = @id", conn);
+                    cmdS.Parameters.AddWithValue("@id", id); // unesemo u URL id koji smo definirali kao @id, koji ce bit usporedjen sa Id iz Film tablice
+                    conn.Open();
+
+                    SqlDataReader reader = cmdS.ExecuteReader();
+
+                    //List<FilmClass> filmList = new List<FilmClass>(); // ne treba lista jel vracamo jedan rezultat
+
+                    if (reader.HasRows)
+                    {
+                        SqlCommand cmdU = new SqlCommand("UPDATE Film SET Title = @title, Release = @release, Genre = @genre, Duration = @duration WHERE Id = @id;", conn);
+
+                        //film.Id = new Guid();
+                        cmdU.Parameters.AddWithValue("@id", id);
+                        cmdU.Parameters.AddWithValue("@title", film.Title = string.IsNullOrWhiteSpace(film.Title) ? "@title" : film.Title);
+                        cmdU.Parameters.AddWithValue("@release", film.Release); //pitat kako za int napravit provjeru
+                        cmdU.Parameters.AddWithValue("@genre", film.Genre = string.IsNullOrWhiteSpace(film.Genre) ? "@genre" : film.Genre);
+                        cmdU.Parameters.AddWithValue("@duration", film.Duration);
+                        
+                        reader.Close();
+
+                        if (cmdU.ExecuteNonQuery() > 0)
+                        {
+                            return Request.CreateResponse(HttpStatusCode.OK, "Updated movie " + film.Title);
+                        }
+                        else
+                        {
+                            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Nothing updated, something went wrong!");
+                        }
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Could not find movie!");
+                    }
+                }   // zatvorio conn, prestao using                        
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error occured while executing Put: {ex.Message}");
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/test/delete/{id}")]
+        public HttpResponseMessage Delete(Guid id)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                using (conn)
+                {
+                    SqlCommand cmdS = new SqlCommand("SELECT * FROM Film WHERE Id = @id", conn);
+                    cmdS.Parameters.AddWithValue("@id", id); // unesemo u URL id koji smo definirali kao @id, koji ce bit usporedjen sa Id iz Film tablice
+                    conn.Open();
+
+                    SqlDataReader reader = cmdS.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        SqlCommand cmdU = new SqlCommand("DELETE FROM Film WHERE Id = @id;", conn);
+
+                        cmdU.Parameters.AddWithValue("@id", id);
+                        
+                        reader.Close();
+
+                        if (cmdU.ExecuteNonQuery() > 0)
+                        {
+                            return Request.CreateResponse(HttpStatusCode.OK, "Movie deleted");
+                        }
+                        else
+                        {
+                            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Something went wrong, did not delete!");
+                        }
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Could not find movie!");
+                    }
+                }   // zatvorio conn, prestao using                        
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error occured while executing Delete: {ex.Message}");
+            }
+        }
 
     }  
 }
