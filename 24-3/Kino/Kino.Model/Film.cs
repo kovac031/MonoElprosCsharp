6c0461f63b05af4e3b636fbaa8b1b3e4a07f55fd
VREMENSKISTROJ\IVAN <ivan.kovac.email@gmail.com>
2023-03-24 08:23:27 +0100

    copied to todays folder

---
+++ 24-3/Kino/Kino.Model/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino.Model
+{
+    public class Film
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Release { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+    }
+}
---
+++ 24-3/Kino/Kino.Repository/FilmRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Kino.Model;
+
+namespace Kino.Repository
+{
+    public class FilmRepository
+    {
+        public static string connectionString = "Data Source=VREMENSKISTROJ;Initial Catalog=SmallCinema;Integrated Security=True";
+
+        public List<Film> GetAll() // vracam listu pa zato
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString); //conn je connection
+
+                using (conn)
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Film", conn);
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //Kino.Model.Film film = new Kino.Model.Film(); // ne treba, vidi ga
+
+                    List<Film> filmList = new List<Film>();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Film film = new Film();
+
+                            film.Id = reader.GetGuid(0);
+                            film.Title = reader.GetString(1);
+                            film.Release = reader.GetInt32(2);
+                            film.Genre = reader.GetString(3);
+                            film.Duration = reader.GetInt32(4);
+
+                            filmList.Add(film);
+                        }
+                        reader.Close();
+                        return filmList;
+                    }
+                    else
+                    {
+                        return (null);
+                    }
+                }   // zatvorio conn, prestao using                        
+            }
+            catch (Exception)
+            {
+                return (null);
+            }
+        }
+    }
+}
